59c80ff2a7c38c53c07b8e31a5389215ae787353
Petr Katerinak <petrkaterinak@inove.cz>
2022-12-20 05:46:38 +0100

    button template fix

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -3,7 +3,7 @@
     $type = !in_array($type, ['button', 'submit', 'reset'], true) ? 'button' : $type;
     $buttonStyle = App\Enums\ButtonStyle::tryfrom($button) ?: App\Enums\ButtonStyle::PRIMARY;
 @endphp
-<button {{ $attributes->merge(['class' => $buttonStyle->getClass() . ' ' . (isset($awesomeIcon) && !isset($wide) ? 'pl-10' : '') . ' ' . (isset($wide) ? 'w-full' : '') . ' items-center group whitespace-nowrap relative flex justify-center py-2 px-4 text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2']) }} {!! isset($click) ? 'onClick="' . $click . '"' : '' !!} type="{{ $type }}">
+<button {{ $attributes->merge(['class' => $buttonStyle->getClass() . ' ' . (isset($awesomeIcon) && !isset($wide) ? 'pl-10' : '') . ' ' . (isset($wide) ? 'w-full' : '') . ' items-center group whitespace-nowrap relative flex justify-center py-2 px-4 text-sm font-medium ' . ($button !== 'text' ? 'focus:outline-none focus:ring-2 focus:ring-offset-2' : '') ]) }} {!! isset($click) ? 'onClick="' . $click . '"' : '' !!} type="{{ $type }}">
     @if(isset($awesomeIcon))
         <span class="absolute inset-y-0 left-0 flex items-center pl-3"><i class="{{ $awesomeIcon }}"></i></span>
     @endif
